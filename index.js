97f016297236022574cc352b1f8ca6445a267594
virgiliusnanamanek <vickynana034@gmail.com>
2022-08-14 17:28:58 +0700

    update index.js

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ class CarFactory {
                 return new Car(4, "v4", "red");
             case "honda":
                 return new Car(2, "v6", "blue");
+
+            case "ford":
+                return new Car(4, "v8", "black");
+
+            case "toyota":
+                return new Car(4, "v6", "white");
+
+            case "nissan":
+                return new Car(4, "v6", "black");
+
+            case "mercedes":
+                return new Car(4, "v8", "red");
         }
     }
 }
@@ -20,6 +32,16 @@ class CarFactory {
 const carFactory = new CarFactory();
 
 const myCivic = carFactory.createCar("civic");
+const myHonda = carFactory.createCar("honda");
+const myFord = carFactory.createCar("ford");
+const myToyota = carFactory.createCar("toyota");
+const myNissan = carFactory.createCar("nissan");
+const myMercedes = carFactory.createCar("mercedes");
 
 console.log(myCivic);
+console.log(myHonda);
+console.log(myFord);
+console.log(myToyota);
+console.log(myNissan);
+console.log(myMercedes);
 
